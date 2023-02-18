e8dc221b643434637fb49bfebd6f4ce4a96caf28
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-02-18 03:28:08 -0300

    feat(nvim): connect wsl clipboard to windows's

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,11 @@
+local has = vim.fn.has
+local is_wsl = has "wsl"
+local is_win = has "win32"
+
+if is_wsl then require('rafaelledo.wsl') end
+if is_win then require('rafaelledo.windows') end
+
 require('rafaelledo.base')
 require('rafaelledo.highlights')
 require('rafaelledo.maps')
 require('rafaelledo.plugins')
-require('rafaelledo.windows')
