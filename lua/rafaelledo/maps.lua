3a3b928f45746e4987acfe1b6d0ecbef6b788b31
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-02-18 21:10:46 -0300

    chore(fish): update maps.lua

--- lua/rafaelledo/maps.lua
+++ lua/rafaelledo/maps.lua
@@ -15,17 +15,15 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 -- Save with root permission (not working for now)
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
--- New tab
-keymap.set('n', 'te', ':tabedit', { silent = true })
--- Close tab
-keymap.set('n', 'tc', ':tabclose', { silent = true })
--- Next tab
-keymap.set('n', 'tn', ':tabnext', { silent = true })
--- Previous tab
-keymap.set('n', 'tp', ':tabprevious', { silent = true })
+keymap.set('n', 'te', ':tabedit', { silent = true }) -- New Tab
+keymap.set('n', 'tc', ':tabclose', { silent = true }) -- Close Tab
+keymap.set('n', 'tn', ':tabnext', { silent = true }) -- Next tab
+keymap.set('n', 'tp', ':tabprevious', { silent = true }) -- Previous tab
+
 -- Split window
 keymap.set('n', 'ss', ':split<Return><C-w>w')
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
 -- Move window
 keymap.set('n', '<Space>', '<C-w>w')
 keymap.set('', 'sh', '<C-w>h')
