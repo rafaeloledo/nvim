88298943bc417b329f053966ec3874f78a9db5c5
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-02-18 21:20:46 -0300

    feat(nvim): change lualine.rc.lua

--- after/plugin/lualine.rc.lua
+++ after/plugin/lualine.rc.lua
@@ -15,11 +15,12 @@ lualine.setup {
     lualine_c = { {
       'filename',
       file_status = true, -- displays file status (readonly status, modified status)
-      path = 0 -- 0 = just filename, 1 = relative path, 2 = absolute path
+      path = 2 -- 0 = just filename, 1 = relative path, 2 = absolute path
     } },
     lualine_x = {
       { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
         hint = ' ' } },
+      'filesize',
       'encoding',
       'filetype'
     },
