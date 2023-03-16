fe53addb8f1a9e1066df909688aeb27a49b9df11
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-03-16 04:55:28 -0300

    feat(lspconfig): add clangd support

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -80,6 +80,10 @@ nvim_lsp.sourcekit.setup {
   capabilities = capabilities,
 }
 
+nvim_lsp.clangd.setup {
+
+}
+
 nvim_lsp.lua_ls.setup {
   capabilities = capabilities,
   on_attach = function(client, bufnr)
@@ -92,7 +96,6 @@ nvim_lsp.lua_ls.setup {
         -- Get the language server to recognize the `vim` global
         globals = { 'vim' },
       },
-
       workspace = {
         -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file("", true),
@@ -119,11 +122,11 @@ nvim_lsp.astro.setup {
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
-  underline = true,
-  update_in_insert = false,
-  virtual_text = { spacing = 4, prefix = "●" },
-  severity_sort = true,
-}
+    underline = true,
+    update_in_insert = false,
+    virtual_text = { spacing = 4, prefix = "●" },
+    severity_sort = true,
+  }
 )
 
 -- Diagnostic symbols in the sign column (gutter)
