2550e739888a8bd6c6e5ed3153573d2de83d7e10
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-06-06 10:44:10 -0300

    chore(nvim): update config

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,8 +1,12 @@
+require('rose-pine').setup({
+  disable_background = true
+})
+
 function ColorMyPencils(color)
   color = color or "rose-pine"
   vim.cmd.colorscheme(color)
 
-  vim.api.nvim_set_hl(0, "Normal", { bg = "#0f0f0f" })
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 end
 
 ColorMyPencils()
