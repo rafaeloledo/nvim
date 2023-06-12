837d8fdcedc4194858d278858759892aaa74c2ec
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-06-12 20:41:55 -0300

    chore(nvim): remove file_browser

--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -3,33 +3,33 @@ if (not status) then return end
 -- local actions = require('telescope.actions')
 local builtin = require("telescope.builtin")
 
-local function telescope_buffer_dir()
-  return vim.fn.expand('%:p:h')
-end
+-- local function telescope_buffer_dir()
+--  return vim.fn.expand('%:p:h')
+-- end
 
-local fb_actions = require "telescope".extensions.file_browser.actions
+-- local fb_actions = require "telescope".extensions.file_browser.actions
 
-telescope.setup {
-  extensions = {
-    file_browser = {
-      hijack_netrw = true,
-      mappings = {
-        ["i"] = {
-          ["<C-w>"] = function() vim.cmd('normal vbd') end,
-        },
-        ["n"] = {
-          ["N"] = fb_actions.create,
-          ["h"] = fb_actions.goto_parent_dir,
-          ["/"] = function()
-            vim.cmd('startinsert')
-          end
-        },
-      },
-    },
-  },
-}
+-- telescope.setup {
+--   extensions = {
+--     file_browser = {
+--       hijack_netrw = true,
+--       mappings = {
+--         ["i"] = {
+--           ["<C-w>"] = function() vim.cmd('normal vbd') end,
+--         },
+--         ["n"] = {
+--           ["N"] = fb_actions.create,
+--           ["h"] = fb_actions.goto_parent_dir,
+--           ["/"] = function()
+--             vim.cmd('startinsert')
+--           end
+--         },
+--       },
+--     },
+--   },
+-- }
 
-telescope.load_extension("file_browser")
+-- telescope.load_extension("file_browser")
 
 local keymap = vim.keymap
 
@@ -39,6 +39,8 @@ keymap.set('n', '<leader>pf', function()
     hidden = true
   })
 end)
+
+keymap.set('n', '<C-p>', builtin.git_files, {})
 
 keymap.set('n', '<leader>ps', function()
   builtin.live_grep()
