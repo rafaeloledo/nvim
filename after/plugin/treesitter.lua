bee4b3a3f4214dcadc8d8716cde2a719fdbfdbf6
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-06-12 23:28:34 -0300

    chore(lua): update config

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -8,6 +8,7 @@ ts.setup {
   sync_install = false,
   auto_install = true,
   ensure_installed = {
+    "vimdoc",
     "python",
     "markdown",
     "markdown_inline",
--- lua/rafaelledo/base.lua
+++ lua/rafaelledo/base.lua
@@ -1,5 +1,7 @@
 -- vim.cmd("autocmd!")
 
+vim.opt.guicursor = ""
+
 vim.scriptencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
