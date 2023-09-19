f000670e0549ba8405b425ac4cf77f6a5875697d
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-09-19 00:26:41 -0300

    chore(nvim): remove unused code; add visual line mapping; kp delete

--- lua/rafaelledo/maps.lua
+++ lua/rafaelledo/maps.lua
@@ -1,81 +1,72 @@
 local keymap = vim.keymap
-local opts = { noremap = true, silent = true }
 
-keymap.set({ 'n', 'v' }, 'x', '"_x', opts) -- no overwrite system clipboard
-keymap.set({ 'n', 'v' }, 'd', '"_d', opts)
-keymap.set('n', 'y', 'vy', opts)
-keymap.set('n', '.', '<nop>', opts)
-
-keymap.set('v', 'd', '"_d', opts)
-keymap.set('v', '<Del>', '"_d', opts)
+keymap.set({ 'n', 'v', 'x' }, 'x', '"_x')
+keymap.set({ 'n', 'v', 'x' }, 'd', '"_d')
+keymap.set({ 'n', 'v', 'x' }, '<Delete>', '"_d')
+keymap.set({ 'n', 'v', 'x' }, '<kDel>', '"_d') -- Key Pad Delete
+keymap.set('n', 'y', 'vy')
+keymap.set('n', '.', '<nop>')
 
 -- Increment/decrement
-keymap.set('n', '+', '<C-a>', opts)
-keymap.set('n', '-', '<C-x>', opts)
+keymap.set('n', '+', '<C-a>')
+keymap.set('n', '-', '<C-x>')
 
 -- Delete a word backwards
-keymap.set('n', 'dw', 'vb"_d', opts)
+keymap.set('n', 'dw', 'vb"_d')
 
 -- Select all
-keymap.set('n', '<C-a>', 'gg<S-v>G', opts)
-
--- Save with root permission (not working for now)
---vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
+keymap.set('n', '<C-a>', 'gg<S-v>G')
 
 -- New tab
-keymap.set('n', 'te', ':tabedit<Return>', opts)
-keymap.set('n', 'q', ':tabclose<Return>', opts)
+keymap.set('n', 'te', ':tabedit<Return>')
+keymap.set('n', 'q', ':tabclose<Return>')
 
 -- Split window
-keymap.set('n', 'ss', ':split<Return><C-w>w', opts)
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w', opts)
+keymap.set('n', 'ss', ':split<Return><C-w>w')
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
 
 -- Move window
-keymap.set('n', '<leader>w', '<C-w>w', opts)
-keymap.set('n', '<leader>h', '<C-w>h', opts)
-keymap.set('n', '<leader>k', '<C-w>k', opts)
-keymap.set('n', '<leader>j', '<C-w>j', opts)
-keymap.set('n', '<leader>l', '<C-w>l', opts)
+keymap.set('n', '<leader>w', '<C-w>w')
+keymap.set('n', '<leader>h', '<C-w>h')
+keymap.set('n', '<leader>k', '<C-w>k')
+keymap.set('n', '<leader>j', '<C-w>j')
+keymap.set('n', '<leader>l', '<C-w>l')
 
 -- Resize window
-keymap.set('n', '<C-w><left>', '<C-w><', opts)
-keymap.set('n', '<C-w><right>', '<C-w>>', opts)
-keymap.set('n', '<C-w><up>', '<C-w>+', opts)
-keymap.set('n', '<C-w><down>', '<C-w>-', opts)
+keymap.set('n', '<C-w><left>', '<C-w><')
+keymap.set('n', '<C-w><right>', '<C-w>>')
+keymap.set('n', '<C-w><up>', '<C-w>+')
+keymap.set('n', '<C-w><down>', '<C-w>-')
 
 -- Indenting remaps
-keymap.set('v', '<TAB>', '>gv', opts)
-keymap.set('v', '<S-TAB>', '<gv', opts)
+keymap.set('v', '<TAB>', '>gv')
+keymap.set('v', '<S-TAB>', '<gv')
 
 -- Allow moving selected lines in visual mode
-keymap.set('v', 'J', ":m '>+1<CR>gv=gv", opts)
-keymap.set('v', 'K', ":m '<-2<CR>gv=gv", opts)
+keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
 -- Allow remove line break without changing cursor position
-keymap.set('n', 'J', "mzJ`z", opts)
+keymap.set('n', 'J', "mzJ`z")
 
 -- Jump page with cursor in middle
-keymap.set('n', '<C-d>', '<C-d>zz', opts)
-keymap.set('n', '<C-u>', '<C-u>zz', opts)
+keymap.set('n', '<C-d>', '<C-d>zz')
+keymap.set('n', '<C-u>', '<C-u>zz')
 
 -- Search with cursor in middle
-keymap.set('n', 'n', 'nzzzv', opts)
-keymap.set('n', 'N', 'Nzzzv', opts)
+keymap.set('n', 'n', 'nzzzv')
+keymap.set('n', 'N', 'Nzzzv')
 
--- nha
-keymap.set('i', '<C-c>', '<Esc>', opts)
-
-keymap.set("n", "<C-f>", vim.lsp.buf.format, opts)
-
-keymap.set('n', 'ec', ':e ~/.config/nvim<Return>', opts)
-keymap.set('n', '<leader>r', ':w<Return>:so<Return>', opts)
+keymap.set('i', '<C-c>', '<Esc>')
+keymap.set("n", "<C-f>", vim.lsp.buf.format)
+keymap.set('n', 'ec', ':e ~/.config/nvim<Return>')
+keymap.set('n', '<leader>r', ':w<Return>:so<Return>')
 keymap.set('n', '<leader>e', function()
   if vim.api.nvim_buf_get_option(0, 'filetype') == 'NvimTree' then
     vim.cmd('NvimTreeClose')
   else
     vim.cmd('NvimTreeOpen')
   end
-end, opts)
-
-keymap.set('i', '<C-z>', '<Esc>u', opts)
-keymap.set('n', '<C-z>', 'u', opts)
+end)
+keymap.set('i', '<C-z>', '<Esc>u')
+keymap.set('n', '<C-z>', 'u')
