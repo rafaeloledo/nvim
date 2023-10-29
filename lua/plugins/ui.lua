03b0d7e2de4dad958c48047d6ce421255021033a
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-10-29 01:15:39 -0300

    chore: make auto

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -20,7 +20,7 @@ return {
       lualine.setup {
         options = {
           icons_enabled = true,
-          theme = 'solarized_dark',
+          theme = 'auto',
           section_separators = { left = '', right = '' },
           component_separators = { left = '', right = '' },
           disabled_filetypes = {}
