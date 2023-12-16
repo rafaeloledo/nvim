5d22b9f2191e8c37592f14c9d920499fcf427249
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-12-16 19:26:27 -0300

    chore(nvim): update base

--- lua/base.lua
+++ lua/base.lua
@@ -1,9 +1,15 @@
 local opt = vim.opt
 local api = vim.api
+
+vim.g.mapleader = " "
+
+opt.guicursor = "i:block"
+
 vim.cmd("language en_US.utf8")
 vim.scriptencoding = "utf-8"
 opt.encoding = "utf-8"
 opt.fileencoding = "utf-8"
+
 vim.wo.number = true
 opt.title = true
 opt.autoindent = true
@@ -15,7 +21,6 @@ opt.cmdheight = 1
 opt.laststatus = 2
 opt.expandtab = true
 opt.scrolloff = 10
--- opt.shell = 'fish'
 opt.inccommand = "split"
 opt.ignorecase = true
 opt.smarttab = true
@@ -29,30 +34,28 @@ opt.winblend = 0
 opt.wildoptions = "pum"
 opt.pumblend = 5
 opt.background = "dark"
--- opt.relativenumber = true
--- highlight yanked text for 200ms using the "Visual" highlight group
-vim.cmd([[
-  augroup highlight_yank
-  autocmd!
-  au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=100})
-  augroup END
-]])
 opt.incsearch = true
 opt.swapfile = false
 vim.wo.signcolumn = "yes"
-vim.g.mapleader = " "
 vim.o.showmode = 1
-vim.cmd("set ffs=unix,dos")
+
+-- opt.relativenumber = true
+-- opt.shell = 'fish'
+
+vim.cmd("set fileformats=unix,dos")
+
 vim.opt.clipboard:prepend({ "unnamed", "unnamedplus" }) -- Merge clipboards
 
 -- Undercurl
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
+
 opt.backupskip = { "/tmp/*", "/private/tmp/*" }
 opt.backspace = { "start", "eol", "indent" }
 opt.path:append({ "**" })
 opt.wildignore:append({ "*/node_modules/*" })
 opt.formatoptions:append({ "r" })
+
 api.nvim_create_autocmd("InsertLeave", {
 	pattern = "*",
 	command = "set nopaste",
