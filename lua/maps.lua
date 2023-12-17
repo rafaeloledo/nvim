b882d5d7dbaee9925174e874810d29b8512be823
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-12-16 22:17:32 -0300

    chore(nvim): update config

--- lua/maps.lua
+++ lua/maps.lua
@@ -9,15 +9,14 @@ set("n", "dw", 'vb"_d') -- Delete word backwards
 set("n", "<C-a>", "gg<S-v>G") -- Select all
 
 set("n", "te", ":tabedit") -- Tab Edit
-set("n", "sv", ":vsplit<Return><C-w>w") -- Split vertically
 set("n", "ss", ":split<cr><C-w>w") -- Split horizontally
+set("n", "sv", ":vsplit<Return><C-w>w") -- Split vertically
 
 -- Move window
-set("n", "<leader>w", "<C-w>w")
-set("n", "<leader>h", "<C-w>h")
-set("n", "<leader>k", "<C-w>k")
-set("n", "<leader>j", "<C-w>j")
-set("n", "<leader>l", "<C-w>l")
+set("n", "sh", "<C-w>h")
+set("n", "sk", "<C-w>k")
+set("n", "sj", "<C-w>j")
+set("n", "sl", "<C-w>l")
 
 -- Resize window
 set("n", "<C-w><left>", "<C-w><")
@@ -26,3 +25,14 @@ set("n", "<C-w><up>", "<C-w>+")
 set("n", "<C-w><down>", "<C-w>-")
 
 set("n", "<S-f>", vim.lsp.buf.format) -- Format
+
+set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+set("n", "K", function() vim.lsp.buf.hover() end, opts)
+set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -129,6 +129,7 @@ return {
 	},
 	{
 		"nvimdev/lspsaga.nvim",
+    enabled = false,
 		config = function()
 			local status, saga = pcall(require, "lspsaga")
 			if not status then
@@ -152,21 +153,21 @@ return {
 
 			local diagnostic = require("lspsaga.diagnostic")
 			local opts = { noremap = true, silent = true }
-			vim.keymap.set("n", "<C-j>", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-			vim.keymap.set("n", "gl", "<Cmd>Lspsaga show_line_diagnostics<CR>", opts)
-			vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opts)
-			vim.keymap.set("n", "gd", "<Cmd>Lspsaga finder<CR>", opts)
-			vim.keymap.set("n", "gt", "<Cmd>Lspsaga goto_type_definition<CR>", opts)
-			-- vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
-			vim.keymap.set("i", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-			vim.keymap.set("n", "<C-k>", function()
-				vim.lsp.buf.signature_help()
-			end, opts)
-			vim.keymap.set("n", "gp", "<Cmd>Lspsaga peek_definition<CR>", opts)
-			vim.keymap.set("n", "gr", "<Cmd>Lspsaga rename<CR>", opts)
-
-			-- code action
-			vim.keymap.set({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+			-- vim.keymap.set("n", "<C-j>", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+			-- vim.keymap.set("n", "gl", "<Cmd>Lspsaga show_line_diagnostics<CR>", opts)
+			-- vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opts)
+			-- vim.keymap.set("n", "gd", "<Cmd>Lspsaga finder<CR>", opts)
+			-- vim.keymap.set("n", "gt", "<Cmd>Lspsaga goto_type_definition<CR>", opts)
+			-- -- vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
+			-- vim.keymap.set("i", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+			-- vim.keymap.set("n", "<C-k>", function()
+			-- 	vim.lsp.buf.signature_help()
+			-- end, opts)
+			-- vim.keymap.set("n", "gp", "<Cmd>Lspsaga peek_definition<CR>", opts)
+			-- vim.keymap.set("n", "gr", "<Cmd>Lspsaga rename<CR>", opts)
+			--
+			-- -- code action
+			-- vim.keymap.set({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
 		end,
 	},
 	{
@@ -189,6 +190,7 @@ return {
 	},
 	{
 		"windwp/nvim-autopairs",
+    enabled = false, -- I'm conversative about the necessity of autopairs
 		event = "InsertEnter",
 		opts = {},
 		init = function()
